02c71ae9fd74884407c25f45549c9ccf0c081e26
foykon <foykon9@gmail.com>
2023-03-08 17:38:03 +0300

    created maven

---
+++ src/main/java/Bit/example/ErrorLoggingService/ErrorLoggingServiceApplication.java
@@ -0,0 +1,13 @@
+package Bit.example.ErrorLoggingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorLoggingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorLoggingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Bit/example/ErrorLoggingService/ErrorLoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Bit.example.ErrorLoggingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorLoggingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

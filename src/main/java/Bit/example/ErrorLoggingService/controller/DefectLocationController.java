05531c5801fdc239b1edb7f35ed7cba3277d81e9
foykon <foykon9@gmail.com>
2023-03-11 19:34:17 +0300

    controller

---
+++ src/main/java/Bit/example/ErrorLoggingService/controller/DefectLocationController.java
@@ -0,0 +1,10 @@
+package Bit.example.ErrorLoggingService.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/DefectLocation")
+public class DefectLocationController {
+
+}

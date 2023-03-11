5d09f8d6d19979d37a8a68abbf1b48b4e53c5e42
foykon <foykon9@gmail.com>
2023-03-11 20:38:22 +0300

    DefectLocationController is fixed

--- src/main/java/Bit/example/ErrorLoggingService/controller/DefectLocationController.java
+++ src/main/java/Bit/example/ErrorLoggingService/controller/DefectLocationController.java
@@ -1,10 +1,22 @@
 package Bit.example.ErrorLoggingService.controller;
 
+import Bit.example.ErrorLoggingService.entity.DefectLocation;
+import Bit.example.ErrorLoggingService.service.DefectLocationService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/DefectLocation")
 public class DefectLocationController {
+    private final DefectLocationService defectLocationService;
 
+    public DefectLocationController(DefectLocationService defectLocationService) {
+        this.defectLocationService = defectLocationService;
+    }
+
+    @PostMapping("/save")
+    public DefectLocation saveDefectLocation(DefectLocation defectLocation){
+        return defectLocationService.saveDefectLocation(defectLocation);
+    }
 }

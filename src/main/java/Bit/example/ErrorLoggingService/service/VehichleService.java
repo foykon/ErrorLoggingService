d6ac0d2b485605df3b45241a37fe5bbdf5189c80
foykon <foykon9@gmail.com>
2023-03-11 20:17:53 +0300

    vehicle service added only save func

---
+++ src/main/java/Bit/example/ErrorLoggingService/service/VehichleService.java
@@ -0,0 +1,7 @@
+package Bit.example.ErrorLoggingService.service;
+
+import Bit.example.ErrorLoggingService.entity.Vehichle;
+
+public interface VehichleService {
+    Vehichle saveVehicle(Vehichle vehichle);
+}
---
+++ src/main/java/Bit/example/ErrorLoggingService/service/impl/VehichleServiceImpl.java
@@ -0,0 +1,18 @@
+package Bit.example.ErrorLoggingService.service.impl;
+
+import Bit.example.ErrorLoggingService.entity.Vehichle;
+import Bit.example.ErrorLoggingService.repository.VehichleRepository;
+import Bit.example.ErrorLoggingService.service.VehichleService;
+
+public class VehichleServiceImpl implements VehichleService {
+    private final VehichleRepository vehichleRepository;
+
+    public VehichleServiceImpl(VehichleRepository vehichleRepository) {
+        this.vehichleRepository = vehichleRepository;
+    }
+
+    @Override
+    public Vehichle saveVehicle(Vehichle vehichle) {
+        return vehichleRepository.save(vehichle);
+    }
+}
